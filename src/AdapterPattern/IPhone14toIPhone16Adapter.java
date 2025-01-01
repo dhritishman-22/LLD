2d5fcc6ecdbf87a0551de101a5ac2a53d83e12ef
dhritishman <dhritishman.kotaki@getvymo.com>
2025-01-02 03:40:52 +0530

    adding adapter design pattern

---
+++ src/AdapterPattern/IPhone14toIPhone16Adapter.java
@@ -0,0 +1,16 @@
+package AdapterPattern;
+
+// Adapter class
+public class IPhone14toIPhone16Adapter implements Charger {
+
+    Iphone14Charger iphone14Charger;
+
+    public IPhone14toIPhone16Adapter() {
+        iphone14Charger = new Iphone14Charger();
+    }
+
+    @Override
+    public void charge() {
+        iphone14Charger.charge();
+    }
+}
---
+++ src/AdapterPattern/Iphone.java
@@ -0,0 +1,6 @@
+package AdapterPattern;
+
+public interface Iphone {
+
+    public void onCharge();
+}
